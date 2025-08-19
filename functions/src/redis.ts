d2040f84b0f742a0cd80f002026d06552c379f7b
Simon Klotz <simon.klotz@datagroup.de>
2025-08-19 13:04:08 +0200

    adds prettier and eslint and reformats code

--- functions/src/redis.ts
+++ functions/src/redis.ts
@@ -1,11 +1,11 @@
-import { defineString } from 'firebase-functions/params';
-import { createClient } from 'redis';
+import { defineString } from "firebase-functions/params";
+import { createClient } from "redis";
 
-const redisUrl = defineString('REDIS_URL');
+const redisUrl = defineString("REDIS_URL");
 
 export async function getRedisClient() {
-    const client = createClient({ url: redisUrl.value() });
-    client.on('error', (err) => console.error('Redis Client Error:', err));
-    await client.connect();
-    return client;
+  const client = createClient({ url: redisUrl.value() });
+  client.on("error", (err) => console.error("Redis Client Error:", err));
+  await client.connect();
+  return client;
 }
--- functions/src/shopify.ts
+++ functions/src/shopify.ts
@@ -1,24 +1,27 @@
-import { defineString } from 'firebase-functions/params';
-import { createStorefrontApiClient } from '@shopify/storefront-api-client';
+import { defineString } from "firebase-functions/params";
+import { createStorefrontApiClient } from "@shopify/storefront-api-client";
 import { Product } from "./models/product.interface";
 import { PageInfo } from "./models/page-info.interface";
 import { ProductResponse } from "./models/product-response.interface";
 
-const shopifyApiVersion = defineString('SHOPIFY_API_VERSION');
-const shopifyStoreDomain = defineString('SHOPIFY_STORE_DOMAIN');
-const shopifyPublicAccessToken = defineString('SHOPIFY_PUBLIC_ACCESS_TOKEN');
+const shopifyApiVersion = defineString("SHOPIFY_API_VERSION");
+const shopifyStoreDomain = defineString("SHOPIFY_STORE_DOMAIN");
+const shopifyPublicAccessToken = defineString("SHOPIFY_PUBLIC_ACCESS_TOKEN");
 
 function getShopifyClient() {
-    return createStorefrontApiClient({
-        storeDomain: shopifyStoreDomain.value(),
-        apiVersion: shopifyApiVersion.value(),
-        publicAccessToken: shopifyPublicAccessToken.value(),
-    });
+  return createStorefrontApiClient({
+    storeDomain: shopifyStoreDomain.value(),
+    apiVersion: shopifyApiVersion.value(),
+    publicAccessToken: shopifyPublicAccessToken.value(),
+  });
 }
 
-export async function fetchProducts(after?: string, first = 10): Promise<{ products: Product[], pageInfo: PageInfo }> {
-    const client = getShopifyClient();
-    const query = `
+export async function fetchProducts(
+  after?: string,
+  first = 10,
+): Promise<{ products: Product[]; pageInfo: PageInfo }> {
+  const client = getShopifyClient();
+  const query = `
       query($first: Int, $after: String) {
         products(first: $first, after: $after) {
           pageInfo {
@@ -58,31 +61,36 @@ export async function fetchProducts(after?: string, first = 10): Promise<{ produ
           }
         }
       }
-    `
+    `;
 
-    const variables = { first, after };
-    const { data, errors } = await client.request<ProductResponse>(query, {variables: variables});
+  const variables = { first, after };
+  const { data, errors } = await client.request<ProductResponse>(query, {
+    variables: variables,
+  });
 
-    if (errors) {
-        console.error(errors);
-        throw new Error(errors.message);
-    }
+  if (errors) {
+    console.error(errors);
+    throw new Error(errors.message);
+  }
 
-    return data ? {
-        products: data.products.edges.map(edge => ({
-            id: edge.node.id,
-            title: edge.node.title,
-            artist: edge.node.artist?.value ?? '',
-            description: edge.node.description,
-            price: Number(edge.node.priceRange.minVariantPrice.amount),
-            imageUrl: edge.node.images.edges[0]?.node.url,
-            audioPreview: edge.node.audioPreview?.value ?? []
+  return data
+    ? {
+        products: data.products.edges.map((edge) => ({
+          id: edge.node.id,
+          title: edge.node.title,
+          artist: edge.node.artist?.value ?? "",
+          description: edge.node.description,
+          price: Number(edge.node.priceRange.minVariantPrice.amount),
+          imageUrl: edge.node.images.edges[0]?.node.url,
+          audioPreview: edge.node.audioPreview?.value ?? [],
         })),
-        pageInfo: data?.products.pageInfo!
-    } : {
+        pageInfo: data?.products.pageInfo!,
+      }
+    : {
         products: [],
         pageInfo: {
-            hasNextPage: false, endCursor: null
-        }
-    }
+          hasNextPage: false,
+          endCursor: null,
+        },
+      };
 }
